761811c40cc0f531033e01a18e201737dadb1491
Makis Tracend <makis.tracend@gmail.com>
2016-03-03 01:08:08 -0800

    + endpoint: .well-known

---
+++ app/controllers/main.js
@@ -0,0 +1,35 @@
+var brisk = require("brisk"),
+	Parent = brisk.getBaseController("main");
+
+var controller = Parent.extend({
+
+	// Let's encrypt verification...
+	".well-known": function(req, res){
+		var letsencrypt = req.site.helpers.letsencrypt;
+		var domain = req.get('host'); // or req.get('origin');
+		// Let's Encrypt
+		//if( req.query["acme-challenge"] ){
+		if( req.query['_key'] == "acme-challenge" ){
+			var creds = letsencrypt.acmeChallenge(domain);
+			if( creds ) res.send( creds );
+			return res.end();
+		}
+		//if( req.query["acme-token"] ){
+		if( req.query['_key'] == "acme-token" ){
+			// save token
+			letsencrypt.acmeToken( req.query["token"], domain );
+			res.send(200); // customize response?
+			return res.end();
+		}
+		// otherwise end the request now
+		return res.end();
+
+	}
+
+});
+
+
+// Helpers
+
+
+module.exports = controller;
